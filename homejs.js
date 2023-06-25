219e1e35843b83ae4d8bfff77222bc0a7aaa8045
rozanmagdy1 <rozanmagdy1@gmail.com>
2023-06-25 14:52:31 +0300

    first commit

---
+++ homejs.js
@@ -0,0 +1,42 @@
+function saturday(){
+    let element1 = document.getElementById('mealImage');
+    element1.src ='images/days_meals/saturdayMeal.jpg';
+    let element2 =document.getElementById('MealDescrip');
+    element2.innerHTML='Grilled beef with onion slices <br><del class="old_price">100$</del>80$';
+}
+function sunday(){
+    let element1 = document.getElementById('mealImage');
+    element1.src ='images/days_meals/sundayMeal.jpg';
+    let element2 =document.getElementById('MealDescrip');
+    element2.innerHTML ='Pasta with meatballs <br><del class="old_price">120$</del>100$';
+}
+function monday(){
+    let element1 = document.getElementById('mealImage');
+    element1.src ='images/days_meals/mondayMeal.jpg';
+    let element2 =document.getElementById('MealDescrip');
+    element2.innerHTML='chicken and red wine <br><del class="old_price">220$</del>170$';
+}
+function tuesday(){
+    let element1 = document.getElementById('mealImage');
+    element1.src ='images/days_meals/tuesdayMeal.jpg';
+    let element2 =document.getElementById('MealDescrip');
+    element2.innerHTML='shrimp and vegetables <br><del class="old_price">200$</del>180$';
+}
+function wednsday(){
+    let element1 = document.getElementById('mealImage');
+    element1.src ='images/days_meals/wednsdayMeal.jpg';
+    let element2 =document.getElementById('MealDescrip');
+    element2.innerHTML='salad and chicken <br><del class="old_price">100$</del>90$';
+}
+function thursday(){
+    let element1 = document.getElementById('mealImage');
+    element1.src ='images/days_meals/thursdayMeal.jpg';
+    let element2 =document.getElementById('MealDescrip');
+    element2.innerHTML='sea food pizza <br><del class="old_price">150$</del>120$';
+}
+function friday(){
+    let element1 = document.getElementById('mealImage');
+    element1.src ='images/days_meals/fridayMeal.jpg';
+    let element2 =document.getElementById('MealDescrip');
+    element2.innerHTML='Burger meal and beer <br><del class="old_price">170$</del>150$';
+}
